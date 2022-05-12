76188fdee1500303bf64ba2113584109bb5eac4e
ahmadhelwa <AHM0173377@ju.edu.jo>
2022-05-12 18:19:36 +0300

    a

--- server.js
+++ server.js
@@ -293,31 +293,32 @@ function hundleNotFound(req,res)
 }
 
 
-// app.get('/', handelhome);
+app.get('/', handelhome);
 
-// function handelhome(req,res)
-// {
-// let result = [];
-// let data1 = new Data(data.title,data.poster_path ,data.overview)
-// result.push(data1);
-// res.json(result);
-// }
+function handelhome(req,res)
+{
+let result = [];
+let data1 = new Data(data.title,data.poster_path ,data.overview)
+result.push(data1);
+res.json(result);
+}
 
 
-// function Data(title,poster_path,overview)
-// {
+function Data(title,poster_path,overview)
+{
   
-  // this.title =title;
-// this.poster_path= poster_path;
-// this.overview= overview;
+  this.title =title;
+this.poster_path= poster_path;
+this.overview= overview;
 
-// }
+}
 
-// app.get('/favorite', handelfavorite)
+app.get('/favorite', handelfavorite)
 
-// function handelfavorite(req,res)
-// {
+function handelfavorite(req,res)
+{
 
-// console.log(res.send("Welcome to Favorite Page"))
+console.log(res.send("Welcome to Favorite Page"))
 
-// 
+
+}
